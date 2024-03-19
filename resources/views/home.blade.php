c06a109e14d08a71c374df0de74dd008dfb5d2ea
lorenzodisanto <150132851+lorenzodisanto@users.noreply.github.com>
2024-03-19 17:11:19 +0100

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Homepage')
+    
+
+@section('main-content')
+    <div class="container py-4">
+      <h1>Laravel with Bootstrap + Vite</h1>
+    </div>
+@endsection
